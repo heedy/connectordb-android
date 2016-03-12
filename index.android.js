b2e324306939a504840e3129976541f04f811743
Daniel Kumor <daniel@dkumor.com>
2016-03-12 16:09:07 -0500

    New setup w/ material design

--- index.android.js
+++ index.android.js
@@ -3,39 +3,82 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
+import React, {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    TextInput,
+    DrawerLayoutAndroid,
+    Image
+} from 'react-native';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  DrawerLayoutAndroid,
-  ToolbarAndroid,
-} = React;
+import {Button, Card, Ripple, Checkbox,} from 'react-native-material-design';
 
-var DeviceView = require("./src/login");
+class connectordb extends Component {
+    render() {
+        return (
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={{
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <Image style={{
+                            width: 100,
+                            height: 100
+                        }} source={require('./branding/square.png')}/>
+                    </View>
+                    <Text style={styles.welcome}>
+                        ConnectorDB
+                    </Text>
+                    <Card>
 
-var ConnectorDB = React.createClass({
-  render: function() {
+                        <Card.Body>
+                            <Text>Log In to ConnectorDB</Text>
+                            <TextInput placeholder="Username" autoCorrect={false} keyboardType="email-address"></TextInput>
+                            <TextInput placeholder="Password" autoCorrect={false} secureTextEntry={true}></TextInput>
+                        </Card.Body>
+                        <Card.Actions position="left">
+                            <Button text="Login" onPress={() => console.log("I pressed a flat button")}/>
+                        </Card.Actions>
+                    </Card>
 
-      var navigationView = (
-          <View style={{flex: 1, backgroundColor: '#fff'}}>
-      <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>Im in the Drawer!</Text>
-      </View>
-      );
+                    <Card>
 
-    return (
-        <DrawerLayoutAndroid drawerWidth={300}
-            drawerPosition={DrawerLayoutAndroid.positions.Left}
-            renderNavigationView={() => navigationView}>
-            <ToolbarAndroid title="ConnectorDB" />
-            <DeviceView path="dkumor/device1"/>
-      </DrawerLayoutAndroid>
-    );
-  }
-});
+                        <Card.Body>
+                            <Text>Server:</Text>
+                            <TextInput placeholder="Server" autoCorrect={false} keyboardType="email-address" value="https://connectordb.com"/>
+                            <Text>Device Name:</Text>
+                            <TextInput placeholder="Device" autoCorrect={false} value="phone"/>
+                            <CheckBox value="gather" label="Gather data when not logged in"/>
+                        </Card.Body>
+                    </Card>
 
-
-
-AppRegistry.registerComponent('ConnectorDB', () => ConnectorDB);
+                </View>
+            </ScrollView>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        backgroundColor: '#F5FCFF',
+    },
+    labeltext: {
+        flex: 1,
+        flexDirection: "row",
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    }
+}
--- ios/ConnectorDBTests/ConnectorDBTests.m
+++ ios/ConnectorDBTests/ConnectorDBTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface ConnectorDBTests : XCTestCase
+@interface connectordbTests : XCTestCase
 
 @end
 
-@implementation ConnectorDBTests
+@implementation connectordbTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
