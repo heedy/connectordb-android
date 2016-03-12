7e0eafa8413fc43fb9eed0531ece53cd43bcd5ce
Daniel Kumor <daniel@dkumor.com>
2016-03-12 16:41:33 -0500

    Upgrade to React-native 0.21

--- android/app/src/main/java/com/connectordb_android/MainActivity.java
+++ android/app/src/main/java/com/connectordb_android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.connectordb;
+package com.connectordb_android;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "connectordb";
+        return "connectordb_android";
     }
 
     /**
@@ -27,14 +27,14 @@ public class MainActivity extends ReactActivity {
         return BuildConfig.DEBUG;
     }
 
-   /**
-   * A list of packages used by the app. If the app uses additional views
-   * or modules besides the default ones, add more packages here.
-   */
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage()
-      );
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage()
+        );
     }
 }
